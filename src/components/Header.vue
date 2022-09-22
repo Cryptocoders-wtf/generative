43c37d2e96e4c2e33399e19d46054747b22eb13c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-21 19:56:44 -0700

    Splatter.svg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
     class="mx-auto flex max-w-xl items-center justify-between py-1.5"
   >
     <div class="flex items-center justify-start gap-1">
-      <img src="@/assets/AssetStoreLogo.png" width="40" height="auto" />
+      <img src="@/assets/Splatter.svg" width="40" height="auto" />
       <div class="text-left font-bold leading-none">
         <div>Fully<br/>On-chain</div>
       </div>
