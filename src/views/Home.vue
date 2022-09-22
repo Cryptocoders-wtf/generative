53b3da0e6e37f8ec4fda9ccd0319dc0fa0076ffe
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-21 20:40:06 -0700

    Home

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mx-auto max-w-3xl p-2 text-left">
+      <p class="mb-2">{{ $t("message.touchToUpdate") }}</p>
+    </div>
+  </template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    export default defineComponent({
+      setup() {
+        return {
+        };
+      },
+    });
+</script>
