a62108727e0c580de73ee440322021af759f82dd
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-23 15:33:31 -0700

    works with goerli

--- src/utils/addresses/splatter_goerli.ts
+++ src/utils/addresses/splatter_goerli.ts
@@ -1,4 +1,4 @@
 export const addresses = {
-  splatterAddress: "0x56d11b0672A387532Dccad55FbabaDF721a4853c",
-  splatterArtAddress: "0x166Fd2A3CAE15C49a2A0752CfedeD9ca7726a7CA",
-};
+  splatterAddress:"0xb1B25Eeb1026cB947b3f65a5cc123FC28B13EEe6",
+  splatterArtAddress:"0x1e9851f8Eec792cDe839277ebB229bb1d45069D9"
+}
