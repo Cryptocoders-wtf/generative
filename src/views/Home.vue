d079b775b78fc0bcf62d02ebcfb2a0beb78e2fd3
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-23 16:07:53 -0700

    updated home page text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <h1 class="mt-2 mb-2 text-xl font-bold">Fully On-Chain Generative Art</h1>
     <p class="mb-2">
       This is a website for developers who are interested in
-      <b>on-chain generative art</b>, which means you write Solidity code to
-      generate art in SVG format.
+      <b>on-chain generative art</b>, which means <u>you write Solidity code to
+      generate art in SVG format</u>.
     </p>
     <p class="mb-2">
       Unlike images stored on HTTP servers or IPFS, on-chain generative arts are
@@ -16,6 +16,7 @@
       Because this is such a new field, I am going to guide you to get there
       step by step.
     </p>
+    <h1 class="mt-2 mb-2 text-xl font-bold">TypeScript/Javascript Prototype</h1>
     <p class="mb-2">
       First of all, please learn how to write JavaScript/TypeScript code to
       generate art in SVG format, which helps you to understand what it takes to
@@ -32,7 +33,7 @@
       yarn run serve</pre
     >
     <p class="mb-2">
-      Then, <code class="bg-gray-200">open "localhost:8080"</code> in your
+      Then, open <code class="bg-gray-200">"localhost:8080"</code> in your
       browser. You'll see the clone of this website.
     </p>
     <p class="mb-2">
@@ -47,12 +48,24 @@
       <code class="bg-gray-200">src/components/Header.vue</code> appropriately
       to add your vue to the menu.
     </p>
+    <h1 class="mt-2 mb-2 text-xl font-bold">Porting it to Solidity</h1>
     <p class="mb-2">
-      I am going to describe how to port your JavaScript/TypeScript code to
-      Solidity later.
+      The Soidity version of Splatter is avaiable at 
+      <code class="bg-gray-200">contract/contracts/SplatterProvider.sol</code>. 
+      Please compare it to TypeScript version. Although there are some differences
+      because of languages, they are build with the same algorithm and similar libraries.
     </p>
     <p class="mb-2">
-      Meanwhile, please join
+      Please notice that the Solidity version generates art deterministically based on the given assetId.
+      This is why some internal functions takes Randomizer.seed as the first parameter and returns the updated one,
+      which creates a pseudo state variable (or a closure) in read-only methods.
+    </p>
+    <p class="mb-2">
+      The Solidity verison also pre-allocate memory (<code class="bg-gray-200">points</code>) 
+      instead of dynamically push elements into the array, which is also necessary to keep those methods read-only. 
+    </p>
+    <p class="mb-2">
+      Please join
       <a href="https://discord.gg/4JGURQujXK" class="underline">our discord</a>,
       and feel free to ask any questions either in English or Japanese.
     </p>
