5ef8925d98e825f1fe35b97c107ac49e2f34bbf5
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-09-26 18:53:58 -0700

    goerli

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -5,12 +5,16 @@ export const getAddresses = (
   const EtherscanBase = (() => {
     if (network == "rinkeby") {
       return "https://rinkeby.etherscan.io/address";
+    } else if (network == "goerli") {
+      return "https://goerli.etherscan.io/address";
     }
     return "https://etherscan.io/address";
   })();
   const OpenSeaBase = (()=>{
     if (network == "rinkeby") {
       return "https://testnets.opensea.io/assets/rinkeby";
+    } else if (network == "goerli") {
+      return "https://testnets.opensea.io/assets/goerli";
     }
     return  "https://opensea.io/assets/ethereum";
   })();
