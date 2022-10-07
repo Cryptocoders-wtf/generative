c810a8cd69a6686f11fed2c392d45f106157aa83
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-07 16:02:02 -0700

    deploy_tokengate

---
+++ contract/scripts/deploy_tokengate.ts
@@ -0,0 +1,33 @@
+import { ethers, network } from "hardhat";
+import { writeFile } from "fs";
+import { whitelistTokens } from "../../src/utils/assetTokens";
+
+const whitelist = whitelistTokens[network.name];
+console.log("whitelist", whitelist);
+
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  const factory = await ethers.getContractFactory("AssetTokenGate");
+  const contract = await factory.deploy();
+  await contract.deployed();
+  console.log(`      tokenGate="${contract.address}"`);
+
+  const tx = await contract.setWhitelist(whitelist);
+  await tx.wait();
+
+  const result2 = await contract.hasWhitelistToken(owner.address);
+  console.log("result2", result2);
+  const result3 = await contract.hasWhitelistToken("0x6a615Ca8D7053c0A0De2d11CACB6f321CA63BD62");
+  console.log("result3", result3);
+
+  const addresses = `export const addresses = {\n`
+    + `  contractArt:"${contract.address}",\n`
+    + `}\n`;
+  await writeFile(`../src/utils/addresses/tokenGate_${network.name}.ts`, addresses, ()=>{});  
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
