244256b37cbbf48676fc5f760972159f9ab816e1
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-09 21:08:01 -0700

    inherit ITokenGate

--- contract/contracts/AssetTokenGate.sol
+++ contract/contracts/AssetTokenGate.sol
@@ -8,15 +8,16 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./interfaces/ITokenGate.sol";
 
-contract AssetTokenGate is Ownable {
+contract AssetTokenGate is Ownable, ITokenGate {
   IERC721[] whitelist;
 
   function setWhitelist(IERC721[] memory _whitelist) external onlyOwner {
     whitelist = _whitelist;
   }
 
-  function balanceOf(address _wallet) external view returns(uint balance) {
+  function balanceOf(address _wallet) external view override returns(uint balance) {
     for (uint i=0; i<whitelist.length; i++) {
       balance += whitelist[i].balanceOf(_wallet);
     }
