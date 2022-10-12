1965e950a524d06d377f21465c33c796149ff0c8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-12 16:02:55 -0700

    TestToken

---
+++ contract/contracts/test/TestToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * Created by Satoshi Nakajima (@snakajima)
+ */
+
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import '../libs/ProviderToken.sol';
+import "../interfaces/ITokenGate.sol";
+
+contract TestToken is ProviderToken {
+  using Strings for uint256;
+
+  constructor(
+    IAssetProvider _assetProvider,
+    IProxyRegistry _proxyRegistry
+  ) ProviderToken(_assetProvider, _proxyRegistry, "Test", "TEST") {
+    description = "This is a part of Fully On-chain Generative Art project (https://fullyonchain.xyz/).";
+    mintPrice = 0;
+    mintLimit = 250;
+  }
+
+  function tokenName(uint256 _tokenId) internal pure override returns(string memory) {
+    return string(abi.encodePacked('Test ', _tokenId.toString()));
+  }
+
+  function mint() public override virtual payable returns(uint256 tokenId) {
+    tokenId = super.mint();
+  }
+}
