32a2023c591b1724d07eb0dce718769e4aa0c3e7
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-13 15:20:21 -0700

    format

--- src/utils/addresses/splatter_goerli.ts
+++ src/utils/addresses/splatter_goerli.ts
@@ -1,6 +1,6 @@
 export const addresses = {
-  svgHelperAddress:"0xbC976E985fbCE68af2847540aBaA2A872e265cbe",
-  splatterAddress:"0x40dcB4e2b4244bCab39F0527C22c9edC473A61Bd",
-  splatterArtAddress:"0x3bA721E0d6079D54201a6a0eB426eC4741a2AD02",
-  splatterToken:"0x4F440891A1831A60FE3B3C813e261a8220aB233a"
-}
+  svgHelperAddress: "0xbC976E985fbCE68af2847540aBaA2A872e265cbe",
+  splatterAddress: "0x40dcB4e2b4244bCab39F0527C22c9edC473A61Bd",
+  splatterArtAddress: "0x3bA721E0d6079D54201a6a0eB426eC4741a2AD02",
+  splatterToken: "0x4F440891A1831A60FE3B3C813e261a8220aB233a",
+};
