8a5dac292f435f027440f3244eeafeb9df99a04e
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-13 21:13:02 -0700

    DummyProxy

---
+++ contract/contracts/test/DummyProxy.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * Created by Satoshi Nakajima (@snakajima)
+ */
+
+pragma solidity ^0.8.6;
+
+import "../external/opensea/IProxyRegistry.sol";
+
+contract DummyProxy is IProxyRegistry {
+  function proxies(address _address) external pure override returns (address) {
+    return _address; // any address is fine
+  }
+}
