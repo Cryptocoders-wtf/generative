bdb1749c85077ff301ca6e8d779e7c5ffea6df5d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-14 14:20:14 -0700

    mainnet.tokenGate

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -4,6 +4,7 @@ import { addresses as snow_goerli } from "./addresses/snow_goerli";
 import { addresses as snow_localhost } from "./addresses/snow_localhost";
 import { addresses as tokenGate_goerli } from "./addresses/tokenGate_goerli";
 import { addresses as tokenGate_localhost } from "./addresses/tokenGate_localhost";
+import { addresses as tokenGate_mainet } from "./addresses/tokenGate_mainnet";
 import { addresses as sample_goerli } from "./addresses/sample_goerli";
 import { addresses as sample_localhost } from "./addresses/sample_localhost";
 import { addresses as nouns_goerli } from "./addresses/nouns_goerli";
@@ -32,6 +33,7 @@ export const addresses: any = {
   tokenGate: {
     goerli: tokenGate_goerli.tokenGate,
     localhost: tokenGate_localhost.tokenGate,
+    mainnet: tokenGate_mainet.tokenGate,
   },
   nouns: {
     goerli: nouns_goerli.providerAddress,
