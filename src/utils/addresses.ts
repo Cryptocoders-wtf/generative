6d8304adde3eb2b47350d8e6605cd4a622442e99
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-14 15:03:21 -0700

    mainnet

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -1,10 +1,11 @@
 import { addresses as splatter_goerli } from "./addresses/splatter_goerli";
 import { addresses as splatter_localhost } from "./addresses/splatter_localhost";
+import { addresses as splatter_mainnet } from "./addresses/splatter_mainnet";
 import { addresses as snow_goerli } from "./addresses/snow_goerli";
 import { addresses as snow_localhost } from "./addresses/snow_localhost";
 import { addresses as tokenGate_goerli } from "./addresses/tokenGate_goerli";
 import { addresses as tokenGate_localhost } from "./addresses/tokenGate_localhost";
-import { addresses as tokenGate_mainet } from "./addresses/tokenGate_mainnet";
+import { addresses as tokenGate_mainnet } from "./addresses/tokenGate_mainnet";
 import { addresses as sample_goerli } from "./addresses/sample_goerli";
 import { addresses as sample_localhost } from "./addresses/sample_localhost";
 import { addresses as nouns_goerli } from "./addresses/nouns_goerli";
@@ -13,14 +14,17 @@ export const addresses: any = {
   svgHelper: {
     goerli: splatter_goerli.svgHelperAddress,
     localhost: splatter_localhost.svgHelperAddress,
+    mainnet: splatter_mainnet.svgHelperAddress,
   },
   splatter: {
     goerli: splatter_goerli.splatterAddress,
     localhost: splatter_localhost.splatterAddress,
+    mainnet: splatter_mainnet.splatterAddress,
   },
   splatterArt: {
     goerli: splatter_goerli.splatterArtAddress,
     localhost: splatter_localhost.splatterArtAddress,
+    mainnet: splatter_mainnet.splatterArtAddress,
   },
   snow: {
     goerli: snow_goerli.snowAddress,
@@ -33,7 +37,7 @@ export const addresses: any = {
   tokenGate: {
     goerli: tokenGate_goerli.tokenGate,
     localhost: tokenGate_localhost.tokenGate,
-    mainnet: tokenGate_mainet.tokenGate,
+    mainnet: tokenGate_mainnet.tokenGate,
   },
   nouns: {
     goerli: nouns_goerli.providerAddress,
