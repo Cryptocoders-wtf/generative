e554a5254589a289e4008352b4d0c9d635a5b4f7
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-14 14:56:50 -0700

    splatter: deployed to mainnet

--- src/utils/addresses/splatter_mainnet.ts
+++ src/utils/addresses/splatter_mainnet.ts
@@ -1,4 +1,6 @@
 export const addresses = {
-  splatterAddress: "0xC32609C91d6B6b51D48f2611308FEf121B02041f",
-  splatterArtAddress: "0x262e2b50219620226C5fB5956432A88fffd94Ba7",
-};
+  svgHelperAddress:"0x9F2dC3f778c07987Fcf5a225c3EFD29400930D50",
+  splatterAddress:"0xFf2738683Eb630cf6322aBf47c449bDc963EA062",
+  splatterArtAddress:"0xB6260e3b97668DCa728d50f4F25C9f824D7BF406",
+  splatterToken:"0x6ef6ca41e5EAe230bf481fFe99Fa231CD8E1d3d8"
+}
