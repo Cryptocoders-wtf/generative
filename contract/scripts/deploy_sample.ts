53d88c04d27d9b7bfb8976e553afa5546eb3781f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-14 09:25:53 -0700

    shared SVGHelper

--- contract/scripts/deploy_sample.ts
+++ contract/scripts/deploy_sample.ts
@@ -2,16 +2,17 @@ import { ethers, network } from "hardhat";
 import { writeFile } from "fs";
 import { addresses } from "../../src/utils/addresses";
 
-export const proxy = "0xa5409ec958c83c3f309868babaca7c86dcb077c1"; // openSea proxy
+const svgHelperAddress = addresses["svgHelper"][network.name];
+console.log("svgHelperAddress", svgHelperAddress);
+
+export const proxy = (network.name == "goerli") ?
+    "0x3143867c145F73AF4E03a13DdCbdB555210e2027": // dummy proxy
+    "0xa5409ec958c83c3f309868babaca7c86dcb077c1"; // openSea proxy
 
 async function main() {
-  const factoryHelper = await ethers.getContractFactory("SVGHelperA");
-  const contractHelper = await factoryHelper.deploy();
-  await contractHelper.deployed();
-  console.log(`      helper="${contractHelper.address}"`);
 
   const factory = await ethers.getContractFactory("SplatterProvider");
-  const contractProvider = await factory.deploy(contractHelper.address);
+  const contractProvider = await factory.deploy(svgHelperAddress);
   await contractProvider.deployed();
   console.log(`      provider="${contractProvider.address}"`);
 
@@ -21,7 +22,6 @@ async function main() {
   console.log(`      token="${token.address}"`);
 
   const addresses = `export const addresses = {\n`
-    + `  svgHelperAddress:"${contractHelper.address}",\n`
     + `  providerAddress:"${contractProvider.address}",\n`
     + `  sampleToken:"${token.address}"\n`
     + `}\n`;
--- src/utils/addresses/sample_goerli.ts
+++ src/utils/addresses/sample_goerli.ts
@@ -1,5 +1,4 @@
 export const addresses = {
-  svgHelperAddress:"0xF7608dD08bdF07229FA3aa5703BFDDe9bc909f25",
-  providerAddress:"0x7072e8bfF38EB37c9b197F7921Fecb84c2ac1121",
-  sampleToken:"0x51CCdac3F0b999bC2d2ab298c3e1Ad5e4BF83842"
+  providerAddress:"0x3Aea980E9F0b6D7e4067A0F3854a0cEfC5F5A957",
+  sampleToken:"0x96Ae594398226d47c9B3b41D3A38a59Db20dfFEf"
 }
