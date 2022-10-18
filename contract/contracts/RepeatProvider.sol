b82adbab954a4a22acbcbb479e1678d220586b1c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-18 15:55:44 -0500

    more adjustment

--- contract/contracts/RepeatProvider.sol
+++ contract/contracts/RepeatProvider.sol
@@ -117,9 +117,9 @@ contract RepeatProvider is IAssetProvider, IERC165, Ownable {
     Properties memory props;
     (seed, props.schemeIndex) = seed.random(schemeCount);
     (seed, props.scheme) = getColorScheme(seed, props.schemeIndex);
-    (seed, props.count) = seed.random(12);
-    props.count += 10;
-    props.sizeVar = 15 - (props.count - 10) / 2;
+    (seed, props.count) = seed.random(14);
+    props.count += 8;
+    props.sizeVar = 16 - (props.count - 8) / 2;
     
     string memory defs;
     string memory tagPart;
