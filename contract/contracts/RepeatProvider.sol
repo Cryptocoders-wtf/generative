3d76968c87d0eaca765c2c8d0a5f2e56283c9e67
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-18 16:01:34 -0500

    better balance

--- contract/contracts/RepeatProvider.sol
+++ contract/contracts/RepeatProvider.sol
@@ -117,9 +117,9 @@ contract RepeatProvider is IAssetProvider, IERC165, Ownable {
     Properties memory props;
     (seed, props.schemeIndex) = seed.random(schemeCount);
     (seed, props.scheme) = getColorScheme(seed, props.schemeIndex);
-    (seed, props.count) = seed.random(14);
-    props.count += 8;
-    props.sizeVar = 16 - (props.count - 8) / 2;
+    (seed, props.count) = seed.random(18);
+    props.count += 6;
+    props.sizeVar = 18 - (props.count - 6) / 2;
     
     string memory defs;
     string memory tagPart;
--- src/utils/addresses/bitcoin_goerli.ts
+++ src/utils/addresses/bitcoin_goerli.ts
@@ -1,5 +1,5 @@
 export const addresses = {
-  assetStoreProvider:"0x08E0481Bd464Bc67DA2c7bA94479ec5721f61716",
-  coinProvider:"0xE627874B34071fbf0C476e40DCB8e34E1F235745",
-  bitcoinArtProvider:"0xc9B10A99356259DC6f38Bc6373d29df1e976129d",
+  assetStoreProvider:"0xBf3EC5f5335772C89563416098a0ee6b9fA153a9",
+  coinProvider:"0xdDdDB29A5cc2CaE9aA623196e148E5280c28A9c7",
+  bitcoinArtProvider:"0xc4C23aACCCB28Df918bd4dEFcD48CB79c5Ec04E7",
 }
