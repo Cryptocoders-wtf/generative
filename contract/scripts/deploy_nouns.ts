5f4281ffd1be51234af0da2b8933985a3d829acc
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-27 09:17:08 -0700

    Matrix for nouns

--- contract/scripts/deploy_nouns.ts
+++ contract/scripts/deploy_nouns.ts
@@ -1,5 +1,8 @@
 import { ethers, network } from "hardhat";
 import { writeFile } from "fs";
+import { addresses } from "../../src/utils/addresses";
+
+const contractSchemes = addresses.colorSchemes[network.name];
 
 const nounsDescriptor:string = (network.name == "goerli") ?
   "0x4d1e7066EEbA8F6c86033F3728C004E71328326D": 
@@ -17,8 +20,8 @@ async function main() {
   await contractProvider.deployed();
   console.log(`      provider="${contractProvider.address}"`);
 
-  const factoryArt = await ethers.getContractFactory("MultiRepeatProvider");
-  const contractArt = await factoryArt.deploy(contractProvider.address, 12, "nounsArt", "Nouns Art");
+  const factoryArt = await ethers.getContractFactory("MatrixProvider");
+  const contractArt = await factoryArt.deploy(contractProvider.address, contractSchemes, 12, "nounsArt", "Nouns Art");
   await contractArt.deployed();
   console.log(`      bitcoinArt="${contractArt.address}"`);  
 
--- src/utils/addresses/nouns_goerli.ts
+++ src/utils/addresses/nouns_goerli.ts
@@ -1,4 +1,4 @@
 export const addresses = {
-  providerAddress: "0xf472d53daAb239259B94FbAF35A905C75caCA345",
-  nounsArt: "0x7010bDCEEE3438a3a1a7EEd184AA9F37cE7f4586",
-};
+  providerAddress:"0x25EAE12451FB9B49560a0498f61E0f432D3D9B69",
+  nounsArt:"0x45910DaaAffbC5E1F2DA13741385092ACD38658D",
+}
