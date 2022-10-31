e8c796c8b667ab7412b762b67bd4e096dea6b786
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-10-31 14:04:56 -0700

    assembly version of concat bytes:

--- contract/contracts/providers/StencilProvider.sol
+++ contract/contracts/providers/StencilProvider.sol
@@ -66,8 +66,31 @@ contract StencilProvider is IAssetProvider, IERC165, Ownable {
   }
 
   function concat(bytes[] memory parts) public pure returns (bytes memory ret) {
+    /*    
     for (uint i = 0; i < parts.length; i++) {
       ret = abi.encodePacked(ret, parts[i]);
+    }
+    */
+    uint count = parts.length;
+    assembly {
+      ret := mload(0x40)
+      let retMemory := add(ret, 0x20)
+      let bufParts := add(parts, 0x20)
+      for {let i := 0} lt(i, count) {i := add(i, 1)} {
+        let src := mload(bufParts) // read the address
+        let dest := retMemory
+        let length := mload(src)
+        // copy 0x20 bytes each (and let it overrun)
+        for {let j := 0} lt(j, length) {j := add(j, 0x20)} {
+          src := add(src, 0x20) // dual purpose
+          mstore(dest, mload(src))
+          dest := add(dest, 0x20)
+        }
+        retMemory := add(retMemory, length)
+        bufParts := add(bufParts, 0x20)
+      }
+      mstore(ret, sub(sub(retMemory, ret), 0x20))
+      mstore(0x40, retMemory)
     }
   }
 
--- src/utils/addresses/circles_localhost.ts
+++ src/utils/addresses/circles_localhost.ts
@@ -1,6 +1,6 @@
 export const addresses = {
-  matrixGenerator:"0x9CC8B5379C40E24F374cd55973c138fff83ed214",
-  contractArt:"0xd3b893cd083f07Fe371c1a87393576e7B01C52C6",
-  contractCircleStencil:"0x3BFbbf82657577668144921b96aAb72BC170646C",
-  contractStencil:"0x930b218f3e63eE452c13561057a8d5E61367d5b7",
+  matrixGenerator:"0xb4e9A5BC64DC07f890367F72941403EEd7faDCbB",
+  contractArt:"0xa8d297D643a11cE83b432e87eEBce6bee0fd2bAb",
+  contractCircleStencil:"0x6Da3D07a6BF01F02fB41c02984a49B5d9Aa6ea92",
+  contractStencil:"0x68d2Ecd85bDEbfFd075Fb6D87fFD829AD025DD5C",
 }
