835e8cba1abc38bffc6cd523c4ca515dbbf2619e
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-05 06:36:06 -0700

    generated

--- src/utils/addresses/circles_localhost.ts
+++ src/utils/addresses/circles_localhost.ts
@@ -1,7 +1,7 @@
 export const addresses = {
-  matrixGenerator:"0x6c383Ef7C9Bf496b5c847530eb9c49a3ED6E4C56",
-  contractArt:"0xAAF0F531b7947e8492f21862471d61d5305f7538",
-  contractCircleStencil:"0x2aA12f98795E7A65072950AfbA9d1E023D398241",
-  contractStencil:"0x81f4f47aa3bBd154171C877b4d70F6C9EeCAb216",
-  contractGlassesStencil:"0x2ce1F0e20C1f69E9d9AEA83b25F0cEB69e2AA2b5",
+  matrixGenerator:"0x9bDD64340D3CE0607f51bBC7508CA40D45849ab8",
+  contractArt:"0xB354ECF032e9e14442bE590D9Eaee37d2924B67A",
+  contractCircleStencil:"0xC54051689e0931FdCF3e708b665f521f7ab42Fb0",
+  contractStencil:"0x00436c9F57dfFd96cECd129c04D9E488c57266cF",
+  contractGlassesStencil:"0xe4a4B3Bc2787aA913e5b4bbce907e8b213250BDe",
 }
