9d2433f5b257077e0b7bdcbfac4d05052d60eae4
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-06 05:59:46 -0800

    SVGMin

---
+++ contract/contracts/test/SVGMin.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * NounsAssetProvider is a wrapper around NounsDescriptor so that it offers
+ * various characters as assets to compose (not individual parts).
+ *
+ * Created by Satoshi Nakajima (@snakajima)
+ */
+
+pragma solidity ^0.8.6;
+
+import "randomizer.sol/Randomizer.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "../packages/graphics/Path.sol";
+import "../packages/graphics/SVG.sol";
+import "hardhat/console.sol";
+
+contract SVGMin {
+  using Strings for uint256;
+  using Randomizer for Randomizer.Seed;
+  using Vector for Vector.Struct;
+  using Path for uint[];
+  using SVG for SVG.Element;
+
+  function main() external pure returns(bytes memory output) {
+    return SVG.rect(256, 256, 512, 512).svg();
+  }
+}
