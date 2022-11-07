07de24ed35fc989549a7bb33cbeb0b9af3435a6d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-07 06:18:34 -0800

    compressPath for each

--- contract/contracts/packages/fonts/gen.ts
+++ contract/contracts/packages/fonts/gen.ts
@@ -1,5 +1,6 @@
 import { readdirSync, readFileSync, writeFileSync, existsSync } from "fs";
 import { XMLParser } from "fast-xml-parser";
+import { compressPath, solidityString } from "../graphics/pathUtils";
 
 const options = {
   ignoreAttributes: false,
@@ -19,8 +20,18 @@ const main = async () => {
     const obj = parser.parse(xml);
     const svg = obj.svg;
     const viewBox = svg['@_viewBox'].split(' ');
+    const height = parseInt(viewBox[3], 10);
     const width = parseInt(viewBox[2], 10);
-    return width;
+    const element = svg.path;
+    let path;
+    if (element.length > 0) {
+      console.log(element.length);
+      path = element.map((item:any) => item["@_d"]).join('');      
+    } else {
+      path = element["@_d"];      
+    }
+    const bytes = solidityString(compressPath(path, height));
+    return { file, width, height, bytes };
   });
   console.log(output);
 };
