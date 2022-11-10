6dfadc8838538ddde39bcf5b432ba41e2c0ab443
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-10 11:27:28 -0800

    sales comission

--- contract/contracts/libs/ERC721P2P.sol
+++ contract/contracts/libs/ERC721P2P.sol
@@ -20,6 +20,7 @@ abstract contract ERC721P2P is IERC721P2P, ERC721, Ownable {
   mapping (uint256 => uint256) prices;
 
   function setPriceOf(uint256 _tokenId, uint256 _price) external override {
+    require(ownerOf(_tokenId) == msg.sender, "Only the onwer can set the price");
     prices[_tokenId] = _price;
   }
 
@@ -28,8 +29,30 @@ abstract contract ERC721P2P is IERC721P2P, ERC721, Ownable {
   }
 
   function purchase(uint256 _tokenId, address _wallet, address _facilitator) external payable override {
-
+    uint256 price = prices[_tokenId];
+    require(price > 0, "Token is not on sale.");
+    require(msg.value >= price, "Not enough fund");
+    uint256 comission = _processSalesCommission(msg.value, _facilitator);
+    address tokenOwner = ownerOf(_tokenId);
+    _processPayout(tokenOwner, msg.value - comission);
+    _transfer(tokenOwner, _wallet, _tokenId);
   }
+
+  // 2.5% to the facilitator (marketplace)
+  function _processSalesCommission(uint _salesPrice, address _facilitator) internal virtual returns(uint256 comission) {    
+    if (_facilitator != address(0)) {
+      comission = _salesPrice * 25 / 1000; // 2.5%
+      address payable payableTo = payable(_facilitator);
+      payableTo.transfer(comission);
+    }
+  }
+
+  // Subclass needs to override to pay royalties to creator(s)
+  function _processPayout(address _tokenOwner, uint _amount) internal virtual {
+    address payable payableTo = payable(_tokenOwner);
+    payableTo.transfer(_amount);
+  }
+
   function acceptOffer(uint256 _tokenId, IERC721Marketplace _dealer, uint256 _price) external override {
 
   }
