98222c387e99e19de49201cab1a3f2fc125c231b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-09 16:51:28 -0800

    ERC721P2P updated

--- contract/contracts/interfaces/IERC721P2P.sol
+++ contract/contracts/interfaces/IERC721P2P.sol
@@ -14,25 +14,25 @@ pragma solidity ^0.8.6;
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-interface IERC721Dealer {
+interface IERC721Marketplace {
   // Make an offer to a specific token
-  function makeAnOffer(IERC721Dealable _contract, uint256 _tokenId, uint256 _price) external payable;
+  function makeAnOffer(ERC721P2P _contract, uint256 _tokenId, uint256 _price) external payable;
   // Withdraw an offer to a specific token (onlyOfferMaker)
-  function withdrawAnOffer(IERC721Dealable _contract, uint256 _tokenId) external;
+  function withdrawAnOffer(ERC721P2P _contract, uint256 _tokenId) external;
   // Get the current offer to the specifiedToken
-  function getTheBestOffer(IERC721Dealable _contract, uint256 _tokenId) external view 
+  function getTheBestOffer(ERC721P2P _contract, uint256 _tokenId) external view 
       returns(uint256, address);
   // It will call the purchase method of _contract with the specified amount of payment.
-  function acceptOffer(IERC721Dealable _contract, uint256 _tokenId, uint256 _price) external;
+  function acceptOffer(ERC721P2P _contract, uint256 _tokenId, uint256 _price) external;
 }
 
-interface IERC721Dealable is IERC721 {
+interface ERC721P2P is IERC721 {
   // Set the price of the specified token (onlyTokenOwner)
   function setPriceOf(uint256 _tokenId, uint256 _price) external;
   // Get the current price of the specified token
   function getPriceOf(uint256 _tokenId) external view returns(uint256);
   // It will transfer the token and distribute the money, including royalties
-  function purchase(uint256 _tokenId, address _wallet) external payable;
+  function purchase(uint256 _tokenId, address _wallet, address _facilitator) external payable;
   // It sets the price and calls the acceptOffer method of _dealer (onlyTokenOwner)
-  function acceptOffer(uint256 _tokenId, IERC721Dealer _dealer, uint256 _price) external;
+  function acceptOffer(uint256 _tokenId, IERC721Marketplace _dealer, uint256 _price) external;
 }
