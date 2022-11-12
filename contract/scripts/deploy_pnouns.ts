a6e4e4eab91dd0d67fb49bbd0e62c94123f6619d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-12 04:47:46 -1000

    deploy_pnouns

---
+++ contract/scripts/deploy_pnouns.ts
@@ -0,0 +1,31 @@
+import { ethers, network } from "hardhat";
+import { writeFile } from "fs";
+
+const nounsDescriptor:string = (network.name == "goerli") ?
+  "0x4d1e7066EEbA8F6c86033F3728C004E71328326D": 
+  "0x0Cfdb3Ba1694c2bb2CFACB0339ad7b1Ae5932B63";
+console.log("nounsDescriptor", nounsDescriptor);
+
+async function main() {
+  const factoryNouns = await ethers.getContractFactory("NounsAssetProvider");
+  const nouns = await factoryNouns.deploy(nounsDescriptor);
+  await nouns.deployed();
+  console.log(`      provider="${nouns.address}"`);
+
+  const factoryFont = await ethers.getContractFactory("LondrinaSolid");
+  const font = await factoryFont.deploy();
+  await font.deployed();
+
+  const factory = await ethers.getContractFactory("PNounsPrivider");
+  const contract = await factory.deploy(font.address, nouns.address);
+  await contract.deployed();
+  console.log(`      test3="${contract.address}"`);
+
+  const result = await contract.generateSVGPart(0);
+  console.log(result);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
