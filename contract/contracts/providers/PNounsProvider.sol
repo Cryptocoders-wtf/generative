5475e19c5bac4807520e1e1ec2fa8fe382ca0496
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-12 04:26:54 -1000

    circles

--- contract/contracts/providers/PNounsProvider.sol
+++ contract/contracts/providers/PNounsProvider.sol
@@ -59,6 +59,45 @@ contract PNounsPrivider is IAssetProvider, Ownable, IERC165 {
     // nothing to return
   }
 
+  // Hack to deal with too many stack variables
+  struct Stackframe {
+    uint degree;
+    uint distance;
+    uint radius;
+    uint rotate;
+    int x;
+    int y;
+  }
+
+  function circles(uint _assetId, string[] memory idNouns) internal pure returns(SVG.Element memory) {
+    string[4] memory colors = ["red", "green", "yellow", "blue"]; 
+    uint count = 10;
+    SVG.Element[] memory elements = new SVG.Element[](count);
+    Randomizer.Seed memory seed = Randomizer.Seed(_assetId, 0);
+
+    for (uint i=0; i<count; i++) {
+      Stackframe memory stack;
+      (seed, stack.degree) = seed.random(0x4000);
+      (seed, stack.distance) = seed.random(480);
+      (seed, stack.radius) = seed.randomize(150, 70);
+      (seed, stack.rotate) = seed.random(360);
+      stack.distance = stack.distance / (stack.radius / 100 + 1);
+      stack.x = 512 + stack.degree.cos() * int(stack.distance) / Vector.ONE;
+      stack.y = 512 + stack.degree.sin() * int(stack.distance) / Vector.ONE;
+      elements[i] = SVG.group([
+                      SVG.use(idNouns[i % idNouns.length])
+                        .transform(TX.translate(uint(stack.x)-stack.radius, uint(stack.y)-stack.radius)
+                                    .scale1000(1000 * stack.radius / 512)
+                                    .rotate(string(abi.encodePacked(stack.rotate.toString(), ",512,512")))),
+                      SVG.circle(stack.x, 
+                                 stack.y, int(stack.radius + stack.radius/10))
+                        .fill(colors[i % 4])
+                        .opacity("0.333")
+                    ]);
+    }
+    return SVG.group(elements);
+  }
+  
   function generateSVGPart(uint256 _assetId) external pure override returns(string memory svgPart, string memory tag) {
     tag = string(abi.encodePacked("circles", _assetId.toString()));
     svgPart = "abc";
