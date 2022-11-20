f40fbb804c2a5bf194bd01144af1de38059ce5ee
isamu <isamu@to-kyo.to>
2022-11-20 09:29:38 +0900

    replace parser

--- contract/tools/src/gen.ts
+++ contract/tools/src/gen.ts
@@ -1,5 +1,13 @@
-import { readdirSync, readFileSync, writeFileSync, mkdirSync, createWriteStream } from "fs";
-import { XMLParser } from "fast-xml-parser";
+import {
+  readdirSync,
+  readFileSync,
+  writeFileSync,
+  mkdirSync,
+  createWriteStream,
+} from "fs";
+
+import { parse, ElementNode, Node } from "svg-parser";
+
 import {
   compressPath,
   solidityString,
@@ -29,90 +37,82 @@ interface SVGData {
   "@_id": string;
 }
 
-const circle2path = (svgData: SVGData) => {
-  const cx = Number(svgData["@_cx"]);
-  const cy = Number(svgData["@_cy"]);
-  const r = Number(svgData["@_r"]);
-  return  `M ${cx} ${cy} m ${-r}, 0 a ${r},${r} 0 1,1 ${r * 2},0 a ${r},${r} 0 1,1 ${-(r * 2)},0`;
-}
+const circle2path = (element: ElementNode) => {
+  const cx = Number(element.properties?.cx);
+  const cy = Number(element.properties?.cy);
+  const r = Number(element.properties?.r);
+  return `M ${cx} ${cy} m ${-r}, 0 a ${r},${r} 0 1,1 ${
+    r * 2
+  },0 a ${r},${r} 0 1,1 ${-(r * 2)},0`;
+};
 
-const polygon2path = (svgData: SVGData) => {
-  const points = svgData["@_points"].split(/\s+|,/);
+const polygon2path = (element: ElementNode) => {
+  const points = ((element.properties?.points as string) || "").split(/\s+|,/);
   const x0 = points.shift();
   const y0 = points.shift();
   return "M" + x0 + "," + y0 + "L" + points.join(" ") + "z";
-}
+};
 
-const ellipse2path = (svgData: SVGData) => {
-  const cx = Number(svgData["@_cx"]);
-  const cy = Number(svgData["@_cy"]);
-  const rx = Number(svgData["@_rx"]);
-  const ry = Number(svgData["@_ry"]);
-  return  `M ${cx} ${cy} m ${-rx}, 0 a ${rx},${ry} 0 1,0 ${rx * 2},0 a ${rx},${ry} 0 1,0 ${-(rx * 2)},0`;
-}
+const ellipse2path = (element: ElementNode) => {
+  const cx = Number(element.properties?.cx);
+  const cy = Number(element.properties?.cy);
+  const rx = Number(element.properties?.rx);
+  const ry = Number(element.properties?.ry);
+  return `M ${cx} ${cy} m ${-rx}, 0 a ${rx},${ry} 0 1,0 ${
+    rx * 2
+  },0 a ${rx},${ry} 0 1,0 ${-(rx * 2)},0`;
+};
 
-const rect2path = (svgData: SVGData) => {
-  const x = Number(svgData["@_x"] || "0");
-  const y = Number(svgData["@_y"] || "0");
-  const rx = Number(svgData["@_width"]);
-  const ry = Number(svgData["@_height"]);
-  return  `M ${x} ${y} H ${rx + x} V ${ry + y} H ${x} Z`;
-}
+const rect2path = (element: ElementNode) => {
+  const x = Number(element.properties?.x || "0");
+  const y = Number(element.properties?.y || "0");
+  const rx = Number(element.properties?.width);
+  const ry = Number(element.properties?.height);
+  return `M ${x} ${y} H ${rx + x} V ${ry + y} H ${x} Z`;
+};
 
+const findPath = (obj: ElementNode[]) => {
+  const ret: ElementNode[] = [];
 
-const findPath = (obj: SVGObj) => {
-  const ret: SVGData[] = [];
-
-  if (Array.isArray(obj)) {
-    obj.map((svgObj) => {
-      findPath(svgObj).map((svgData) => {
-        ret.push(svgData);
+  obj.map((element) => {
+    if (element.children) {
+      findPath(element.children as ElementNode[]).map((childRet) => {
+        ret.push(childRet);
       });
-    });
-  } else {
-    Object.keys(obj).map((key) => {
-      if (key === "path") {
-        (Array.isArray(obj.path) ? obj.path : [obj.path]).map((svgData: SVGData) => {
-          if (svgData["@_style"]) {
-            // console.log(svgData["@_style"])
-          }
-          ret.push(svgData);
-        });
-      } else if (key === "circle") {
-        (Array.isArray(obj.circle) ? obj.circle : [obj.circle]).map((svgData: SVGData) => {
-          svgData["@_d"] = circle2path(svgData);
-          ret.push(svgData);
-        });
-      } else if (key === "ellipse") {
-        (Array.isArray(obj.ellipse) ? obj.ellipse : [obj.ellipse]).map((svgData: SVGData) => {
-          svgData["@_d"] = ellipse2path(svgData);
-          ret.push(svgData);
-        });
-      } else if (key === "rect") {
-        (Array.isArray(obj.rect) ? obj.rect : [obj.rect]).map((svgData: SVGData) => {
-          svgData["@_d"] = rect2path(svgData);
-          ret.push(svgData);
-        });
-      } else if (key === "polygon") {
-        (Array.isArray(obj.polygon) ? obj.polygon : [obj.polygon]).map((svgData: SVGData) => {
-          svgData["@_d"] = polygon2path(svgData);
-          ret.push(svgData);
-        });
-      } else if (key === "clipPath") {
-        // skip
-      } else {
-        if (typeof obj[key as keyof SVGData] === "object") {
-          findPath(obj[key as keyof SVGData] as SVGObj).map((svgData) => {
-            ret.push(svgData);
-          });
-        }
+    }
+    if (element.tagName === "path") {
+      ret.push(element);
+    }
+    if (element.tagName === "circle") {
+      if (element.properties) {
+        element.properties.d = circle2path(element);
       }
-    });
-  }
+      ret.push(element);
+    }
+    if (element.tagName === "ellipse") {
+      if (element.properties) {
+        element.properties.d = ellipse2path(element);
+      }
+      ret.push(element);
+    }
+    if (element.tagName === "rect") {
+      if (element.properties) {
+        element.properties.d = rect2path(element);
+      }
+      ret.push(element);
+    }
+    if (element.tagName === "polygon") {
+      if (element.properties) {
+        element.properties.d = polygon2path(element);
+      }
+      ret.push(element);
+    }
+  });
   return ret;
 };
-const getSvgSize = (svg: SVGData) => {
-  const viewBox = svg["@_viewBox"].split(" ");
+
+const getSvgSize = (svg: ElementNode) => {
+  const viewBox = ((svg.properties?.viewBox as string) || "").split(" ");
   // const originalHeight = parseInt(viewBox[3], 10);
   // const originalWidth = parseInt(viewBox[2], 10)
   const originalHeight = 730;
@@ -120,18 +120,16 @@ const getSvgSize = (svg: SVGData) => {
 
   // 730;
   const width = Math.round((originalWidth * 1024) / originalHeight);
-  console.log({ height: originalHeight, width: width });
-  // return { height, width };
-  return { height: originalHeight*10, width: width*10 };
+  return { height: originalHeight * 10, width: width * 10 };
 };
 
-const dumpConvertSVG = (svg: SVGData, pathElements: SVGData[]) => {
-  const vb = svg["@_viewBox"];
+const dumpConvertSVG = (svg: ElementNode, pathElements: ElementNode[]) => {
+  const vb = (svg.properties?.viewBox as string) || "";
   const ret =
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="${vb}">\n\t<g>\n` +
     pathElements
       .map((svgData) => {
-        const d = svgData["@_d"];
+        const d = svgData.properties?.d;
         return `\t\t<path d="${d}" style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:3px;stroke:#000;" />`;
         //        return `\t\t<path d="${d}"  />`;
       })
@@ -139,22 +137,25 @@ const dumpConvertSVG = (svg: SVGData, pathElements: SVGData[]) => {
     "\n\t</g>\n</svg>\n";
   return ret;
 };
-const dumpConvertSVG2 = (svg: SVGData, paths: any[]) => {
-  const vb = svg["@_viewBox"];
+
+const dumpConvertSVG2 = (svg: ElementNode, paths: any[]) => {
+  const vb = (svg.properties?.viewBox as string) || "";
   const ret =
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="${vb}">\n\t<g>\n` +
     paths
       .map((pathData) => {
         const d = pathData["path"];
-        const fill = pathData["fill"]
+        const fill = pathData["fill"];
         const stroke = pathData["stroke"];
         const styles = [];
         if (fill) {
-          styles.push(`fill:${fill}`)
+          styles.push(`fill:${fill}`);
         }
         if (stroke) {
-          styles.push(`stroke-linecap:round;stroke-linejoin:round;stroke-width:${stroke};stroke:#000`);
-        };
+          styles.push(
+            `stroke-linecap:round;stroke-linejoin:round;stroke-width:${stroke};stroke:#000`
+          );
+        }
         const style = styles.join(";");
         return `\t\t<path d="${d}" style="${style}" />`;
       })
@@ -164,20 +165,13 @@ const dumpConvertSVG2 = (svg: SVGData, paths: any[]) => {
 };
 
 const style2elem = (style: string) => {
-  const styles = style.split(";").map(a => a.split(":"));
+  const styles = style.split(";").map((a) => a.split(":"));
   return styles.reduce((tmp: any, key: string[]) => {
     tmp[key[0]] = key[1];
     return tmp;
   }, {});
 };
-const main = async () => {
-  const options = {
-    ignoreAttributes: false,
-    format: true,
-  };
-  
-  const parser = new XMLParser(options);
-
+const main = async (folder: string) => {
   const files = readdirSync(folder).filter((fileName) => {
     return fileName.endsWith(".svg");
   });
@@ -193,31 +187,34 @@ const main = async () => {
     const name = fileName.split(".")[0];
     const char = name;
 
-    const obj = parser.parse(svgData);
-    const svg = obj.svg;
+    const obj = parse(svgData);
+
+    const svg = obj.children[0] as ElementNode;
     const { height, width } = getSvgSize(svg);
 
-    const pathElements = findPath(svg);
-    if (fileName === "Lu_1.svg")
-      console.log(svg.g.g)
-    const path = pathElements.map((item: SVGData) => {
-      return item["@_d"]
-    }).join("");
+    const pathElements = findPath(svg.children as ElementNode[]);
 
-    const path2 = pathElements.map((item: SVGData) => {
-      const styles = (style2elem(item["@_style"]||""));
+    const path = pathElements
+      .map((item: ElementNode) => {
+        return item.properties?.d;
+      })
+      .join("");
+    //console.log(path)
+
+    const path2 = pathElements.map((item: ElementNode) => {
+      const styles = style2elem((item.properties?.style as string) || "");
       const fill = styles["fill"];
       const stroke = styles["stroke-width"];
       return {
-        path: item["@_d"],
+        path: item.properties?.d || "",
         fill,
-        "stroke": stroke,
-      }
+        stroke: stroke,
+      };
     });
+    console.log(path2);
 
     // const convertedSVG = dumpConvertSVG(svg, pathElements);
     const convertedSVG = dumpConvertSVG2(svg, path2);
-    name + ".svg";
 
     writeFileSync(outdir + "/svgs/" + fileName, convertedSVG);
 
@@ -226,9 +223,14 @@ const main = async () => {
     return { fileName, char, name, width, height, bytes };
   });
 
-  writeFileSync(outdir + "/svgs/index.html", files.map((fileName) => {
-    return `<img src="${fileName}" width="300px" />\n`
-  }).join(""));
+  writeFileSync(
+    outdir + "/svgs/index.html",
+    files
+      .map((fileName) => {
+        return `<img src="${fileName}" width="300px" />\n`;
+      })
+      .join("")
+  );
 
   const stream = createWriteStream(outdir + "/data/data.txt");
   stream.on("error", (err) => {
@@ -260,4 +262,4 @@ if (!folder) {
   process.exit(-1);
 }
 
-main();
+main(folder);
