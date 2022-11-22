a481c55eba367b0ff4266d6d1acaabf2a7ce86fc
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-22 01:22:44 -1000

    dynamic token gate with splatter

--- contract/scripts/deploy_dynamic.ts
+++ contract/scripts/deploy_dynamic.ts
@@ -2,27 +2,42 @@ import { ethers, network } from "hardhat";
 import { writeFile } from "fs";
 import { addresses } from "../../src/utils/addresses";
 
-const splatterToken = addresses.splatterToken[network.name];
+const splatterTokenAddress = addresses.splatterToken[network.name];
 
 async function main() {
+  let result;
   const [owner] = await ethers.getSigners();
+  console.log("owner", owner.address);
+
+  const splatterFactory = await ethers.getContractFactory("SplatterToken");
+  const splatterToken = await splatterFactory.attach(splatterTokenAddress);
+
+  result = await splatterToken.balanceOf(owner.address);
+  console.log("splatter owner", result);
+  result = await splatterToken.balanceOf("0x4E4cD175f812f1Ba784a69C1f8AC8dAa52AD7e2B");
+  console.log("splatter user1", result);
+
+  if (false) {
+    return;
+  }
+
   const factory = await ethers.getContractFactory("DynamicTokenGate");
   const contract = await factory.deploy();
   await contract.deployed();
   console.log(`      tokenGate="${contract.address}"`);
 
-  const result2 = await contract.balanceOf(owner.address);
-  console.log("result2", result2);
-  const result3 = await contract.balanceOf("0x4E4cD175f812f1Ba784a69C1f8AC8dAa52AD7e2B");
-  console.log("result3", result3);
+  result = await contract.balanceOf(owner.address);
+  console.log("result2", result);
+  result = await contract.balanceOf("0x4E4cD175f812f1Ba784a69C1f8AC8dAa52AD7e2B");
+  console.log("result3", result);
 
-  const tx = await contract.append(splatterToken);
+  const tx = await contract.append(splatterTokenAddress);
   await tx.wait();
 
-  const result4 = await contract.balanceOf(owner.address);
-  console.log("result4", result2);
-  const result5 = await contract.balanceOf("0x4E4cD175f812f1Ba784a69C1f8AC8dAa52AD7e2B");
-  console.log("result5", result3);
+  result = await contract.balanceOf(owner.address);
+  console.log("result4", result);
+  result = await contract.balanceOf("0x4E4cD175f812f1Ba784a69C1f8AC8dAa52AD7e2B");
+  console.log("result5", result);
 
   const addresses = `export const addresses = {\n`
     + `  tokenGate:"${contract.address}",\n`
