7d0efb0ea59498e7c5a458e0a633f3ad5210844d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-11-30 07:36:59 +0900

    nouns/dotNouns deployed on goerli

--- src/utils/addresses/nouns_goerli.ts
+++ src/utils/addresses/nouns_goerli.ts
@@ -1,4 +1,3 @@
 export const addresses = {
-  providerAddress:"0x0A9EB455a8835399DcC4EA4C88d313351E6CF28F",
-  nounsArt:"0x90A6da6BD2C65d8c0302Fa61E51407Cb1c65eE42",
+  providerAddress:"0x6eE2a79f4938e95dFF3C18e9aba04025Ce3A6c22",
 }
