4de85c464081512161bdb6fe4e58f1e1ac85cfe4
isamu <isamu@to-kyo.to>
2022-12-06 15:06:46 +0900

    add LuArtProvider

---
+++ contract/contracts/providers/LuArtProvider.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+
+/*
+ * NounsAssetProvider is a wrapper around NounsDescriptor so that it offers
+ * various characters as assets to compose (not individual parts).
+ *
+ * Created by Satoshi Nakajima (@snakajima)
+ */
+
+pragma solidity ^0.8.6;
+
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import "assetprovider.sol/IAssetProvider.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import '@openzeppelin/contracts/interfaces/IERC165.sol';
+// import "../packages/graphics/Path.sol";
+import "../packages/graphics/SVG.sol";
+
+import "../lu/ISVGArt.sol";
+
+contract LuArtProvider is IAssetProvider, IERC165, Ownable {
+  using Strings for uint256;
+  // using Vector for Vector.Struct;
+  // using Path for uint[];
+  using SVG for SVG.Element;
+
+  ISVGArt immutable svgArt;
+
+  string constant providerKey = "LaidbackLu";
+  address public receiver;
+
+  constructor(
+    ISVGArt _svgArt
+  ) {
+    svgArt = _svgArt;
+    receiver = owner();
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+    return
+      interfaceId == type(IAssetProvider).interfaceId ||
+      interfaceId == type(IERC165).interfaceId;
+  }
+
+  function getOwner() external override view returns (address) {
+    return owner();
+  }
+
+  function getProviderInfo() external view override returns(ProviderInfo memory) {
+    return ProviderInfo(providerKey, "Laidback Lu", this);
+  }
+
+  function totalSupply() external pure override returns(uint256) {
+    return 0; // indicating "dynamically (but deterministically) generated from the given assetId)
+  }
+
+  function processPayout(uint256 _assetId) external override payable {
+    address payable payableTo = payable(receiver);
+    payableTo.transfer(msg.value);
+    emit Payout(providerKey, _assetId, payableTo, msg.value);
+  }
+
+  function setReceiver(address _receiver) onlyOwner external {
+    receiver = _receiver;
+  }
+
+  function generateSVGPart(uint256 _assetId) public view override returns(string memory svgPart, string memory tag) {
+    bytes memory path = svgArt.getSVGBody(uint16(_assetId % 96));
+    
+    tag = string(abi.encodePacked(providerKey, _assetId.toString()));
+    svgPart = string(path);
+  }
+
+  function generateTraits(uint256 _assetId) external pure override returns (string memory traits) {
+    // nothing to return
+  }
+
+  // For debugging
+  function generateSVGDocument(uint256 _assetId) external view returns(string memory svgDocument) {
+    string memory svgPart;
+    string memory tag;
+    (svgPart, tag) = generateSVGPart(_assetId);
+    svgDocument = string(SVG.document(
+      "0 0 1024 1024",
+      bytes(svgPart),
+      SVG.use(tag).svg()
+    ));
+  }
+}
