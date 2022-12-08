2f50657ba198b75cea0a30145c0006bc6ca1461f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-12-08 13:02:53 -1000

    nounsV2 on star page

--- src/views/Star.vue
+++ src/views/Star.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mx-auto max-w-3xl p-2 text-left">
     <ProviderView assetProvider="star" debugMode="true" />
+    <ProviderView assetProvider="nounsV2" debugMode="true" network="mainnet" />
   </div>
 </template>
 
