05d998cc289f74ca871697413b760cd52f9384e6
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-12-10 06:13:00 -1000

    simplified

--- contract/contracts/packages/graphics/SVGFilter.sol
+++ contract/contracts/packages/graphics/SVGFilter.sol
@@ -16,14 +16,14 @@ library SVGFilter {
     elem.head = abi.encodePacked('<filter style="color-interpolation-filters:sRGB;" id="', id);
     elem.tail = bytes(
       '" >'
-        '<feTurbulence type="fractalNoise" baseFrequency="0.04" numOctaves="5" seed="0" result="result4" />'
-        '<feDisplacementMap in="SourceGraphic" in2="result4" yChannelSelector="G" xChannelSelector="R" scale="10" result="result3" />'
-        '<feDiffuseLighting lighting-color="rgb(233,230,215)" diffuseConstant="1" surfaceScale="2" result="result1" in="result4" >'
+        '<feTurbulence type="fractalNoise" baseFrequency="0.04" numOctaves="5" seed="0" result="r4" />'
+        '<feDisplacementMap in="SourceGraphic" in2="r4" yChannelSelector="G" xChannelSelector="R" scale="10" result="r3" />'
+        '<feDiffuseLighting lighting-color="rgb(233,230,215)" diffuseConstant="1" surfaceScale="2" result="r1" in="r4" >'
           '<feDistantLight azimuth="235" elevation="40" />'
         '</feDiffuseLighting>'
-        '<feComposite operator="in" in="result3" in2="result1" result="result2" />'
-        '<feComposite in2="result1" result="result5" operator="arithmetic" k1="1.7" />'
-        '<feBlend in="result5" in2="result3" mode="normal" />'
+        '<feComposite operator="in" in="r3" in2="r1" />'
+        '<feComposite in2="r1" operator="arithmetic" k1="1.7" />'
+        '<feBlend in2="r3" mode="normal" />'
       '</filter>'
     );
   }
