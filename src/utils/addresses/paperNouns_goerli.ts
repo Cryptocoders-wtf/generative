5b88c4dc8ee87d5ee6ce44105e1c4fa5c91032b1
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-12-10 14:33:17 -1000

    paperNouns

---
+++ src/utils/addresses/paperNouns_goerli.ts
@@ -0,0 +1,3 @@
+export const addresses = {
+  dotNounsArt:"0x10a1B2d2073a7B487bB97BF565980B9d5051EAf3",
+}
---
+++ src/utils/addresses/paperNouns_localhost.ts
@@ -0,0 +1,3 @@
+export const addresses = {
+  dotNounsArt:"0x6c383Ef7C9Bf496b5c847530eb9c49a3ED6E4C56",
+}
