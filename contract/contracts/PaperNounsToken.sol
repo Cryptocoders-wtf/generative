d844da5c5f7c7b3ae24d0ce6e09f8ada59e797e9
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-12-12 10:06:33 -1000

    lock

--- contract/contracts/PaperNounsToken.sol
+++ contract/contracts/PaperNounsToken.sol
@@ -13,20 +13,41 @@
 pragma solidity ^0.8.6;
 
 import '@openzeppelin/contracts/utils/Strings.sol';
-import './libs/ProviderToken3.sol';
+import './libs/ProviderToken4.sol';
 import './interfaces/ITokenGate.sol';
 
-contract PaperNounsToken is ProviderToken3 {
+contract PaperNounsToken is ProviderToken4 {
   using Strings for uint256;
   ITokenGate public immutable tokenGate;
+  bool public locked = true;
 
   constructor(
     ITokenGate _tokenGate,
     IAssetProvider _assetProvider
-  ) ProviderToken3(_assetProvider, 'Paper Nouns', 'PAPERNOUNS') {
+  ) ProviderToken4(_assetProvider, 'Paper Nouns', 'PAPERNOUNS') {
     tokenGate = _tokenGate;
     description = 'This is a part of Fully On-chain Generative Art project (https://fullyonchain.xyz/). All images are dymically generated on the blockchain.';
     mintPrice = 1e16; //0.01 ether, updatable
+  }
+
+  function setLock(bool _locked) external onlyOwner {
+    locked = _locked;
+  }
+
+  function setApprovalForAll(
+    address operator,
+    bool approved
+  ) public virtual override(ERC721WithOperatorFilter, IERC721) {
+    require(!locked, "The contract is locked during the initial minting.");
+    super.setApprovalForAll(operator, approved);
+  }
+
+  function _beforeTokenTransfer(
+      address,
+      address,
+      uint256
+  ) internal override view {
+    require(!locked, "The contract is locked during the initial minting.");
   }
 
   function tokenName(uint256 _tokenId) internal pure override returns (string memory) {
@@ -55,11 +76,12 @@ contract PaperNounsToken is ProviderToken3 {
   function mintPriceFor(address _wallet) public view virtual override returns (uint256) {
     if (balanceOf(_wallet) == 1) {
       return mintPrice * 2; // x2 for second
-    } else if (balanceOf(_wallet) == 2) {
-      return mintPrice * 4; // x4 for third
     }
     if (tokenGate.balanceOf(_wallet) > 0) {
       return mintPrice / 2; // 50% off
+    }
+    if (locked) {
+      return mintPrice * 4; // x4 while locked
     }
     return mintPrice;
   }
