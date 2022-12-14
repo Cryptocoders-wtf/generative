b99b8cadf67acf51d8b588263be4bbfda076c403
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2022-12-14 13:51:45 -1000

    deployed on goerli

--- contract/contracts/PaperNounsToken.sol
+++ contract/contracts/PaperNounsToken.sol
@@ -20,6 +20,7 @@ contract PaperNounsToken is ProviderToken4 {
   using Strings for uint256;
   ITokenGate public immutable tokenGate;
   bool public locked = true;
+  bool public limited = true;
   IERC721 public dotNouns;
 
   constructor(
@@ -35,6 +36,10 @@ contract PaperNounsToken is ProviderToken4 {
 
   function setLock(bool _locked) external onlyOwner {
     locked = _locked;
+  }
+
+  function setLimited(bool _limited) external onlyOwner {
+    limited = _limited;
   }
 
   // Disable any approve and transfer during the initial minting
@@ -62,7 +67,7 @@ contract PaperNounsToken is ProviderToken4 {
 
   function toBeGifted(uint256 _tokenId) public pure returns(bool) {
     uint256[33] memory list = [uint256(1), 245, 403, 405, 406, 407, 410, 415, 416, 417, 419, 422, 423, 434, 450, 452, 453,
-454, 456, 460, 471, 474, 475, 479, 487, 490, 492, 497, 499, 505, 512, 519, 547];
+454, 456, 460, 471, 474, 475, 479, 487, 490, 492, 497, 499, 505, 512, 519, 543];
     for (uint i = 0; i < list.length; i++) {
       if (list[i] == _tokenId) {
         return true;
@@ -97,16 +102,22 @@ contract PaperNounsToken is ProviderToken4 {
   }
 
   function mintPriceFor(address _wallet) public view virtual override returns (uint256) {
-    if (balanceOf(_wallet) > 0) {
-      return mintPrice * 4; // x4 for second
+    uint256 wlCount = tokenGate.balanceOf(_wallet);
+    uint256 balance = balanceOf(_wallet);
+    // During the private sale
+    if (limited) {
+      if (balance == 0 && wlCount > 0) {
+        return mintPrice / 2; // 50% off for WL holders (only one per wallet)
+      }
+      return mintPrice * 4; // 4x, otherwise
     }
-    if (tokenGate.balanceOf(_wallet) > 0) {
-      return mintPrice / 2; // 50% off
+
+    // Public sale
+    if (balance == 0 || balance < wlCount) {
+      return mintPrice; // standard price for public or WL holder (upto the limit)
     }
-    if (locked) {
-      return mintPrice * 4; // x4 while locked for non-WL holders
-    }
-    return mintPrice;
+
+    return mintPrice * 2; // 200% more beyond the limit
   }
 
   function _processRoyalty(uint _salesPrice, uint _tokenId) internal override returns (uint256 royalty) {
--- src/utils/addresses/paperNounsToken_goerli.ts
+++ src/utils/addresses/paperNounsToken_goerli.ts
@@ -1,3 +1,3 @@
 export const addresses = {
-  token:"0x4463fb918a22c2Dc2904951C200A712AE8F9105d",
+  token:"0xf2b75BE22fd865f7dE4C30f8654CE041Bd1e2FF2",
 }
