9d973dc90fcd7f08520d06cd10aace7d85979865
isamu <isamu@to-kyo.to>
2022-12-23 18:00:43 +0900

    add svgs

--- src/views/SVGTest.vue
+++ src/views/SVGTest.vue
@@ -20,10 +20,24 @@ import lix from "../svgs/Liuxingti.svg?raw";
 import ss from "../svgs/SingularitySociety-LogoType-Color.svg?raw";
 import svg from "../svgs/Sustainable_Development_Goals.svg?raw";
 
+import f1F439 from "../svgs/emoji/1F439.svg?raw";
+import f1F6FC from "../svgs/emoji/1F6FC.svg?raw";
+import f1F94A from "../svgs/emoji/1F94A.svg?raw";
+import f1F989 from "../svgs/emoji/1F989.svg?raw";
+import f1F9A5 from "../svgs/emoji/1F9A5.svg?raw";
+import f1FAE3 from "../svgs/emoji/1FAE3.svg?raw";
+import BurkinaFaso from "../svgs/emoji/BurkinaFaso.svg?raw";
+import E0C5 from "../svgs/emoji/E0C5.svg?raw";
+
+import pnounsNFT_font_question from "../svgs/font/pnounsNFT_font_question.svg?raw";
+
+
 export default defineComponent({
   setup(props) {
     const svgs = [
-      semi, semi2, hanafuda, lix, ss, svg
+      f1F439, f1F6FC, f1F94A, f1F989, f1F9A5, f1FAE3, BurkinaFaso, E0C5, 
+      semi, semi2, hanafuda, lix, ss, svg,
+      pnounsNFT_font_question,
     ].map((svgText) => {
       const convedPath = convSVG2Path(svgText as string, true);
       const convedSVGText = dumpConvertSVG(convedPath);
