06eedf6a544624e693953f9b44cb1a61464eda44
isamu <isamu@to-kyo.to>
2023-03-08 07:29:22 +0900

    add text split test

---
+++ contract/test/message.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+let token: Contract;
+
+before(async () => {
+  const factory = await ethers.getContractFactory('TextSplitTest');
+  token = await factory.deploy();
+});
+
+describe('TextSplit', function () {
+  it('Text 2 Array', async function () {
+    const text = 'this is test';
+    const result = await token.splitOnNewline(text);
+    expect(result.length).equal(1);
+    expect(result).to.deep.equal(text.split('\n'));
+    console.log(result);
+  });
+
+  it('Text with new line 2 Array', async function () {
+    const text = 'This\nis\na\npen';
+    const result = await token.splitOnNewline(text);
+    expect(result.length).equal(4);
+    console.log(result);
+    expect(result).to.deep.equal(text.split('\n'));
+  });
+
+  it('Text sandwiched between newlines 2 Array', async function () {
+    const text = '\nThis\nis\na\npen\n';
+    const result = await token.splitOnNewline(text);
+    console.log(result);
+    expect(result.length).equal(6);
+    expect(result).to.deep.equal(text.split('\n'));
+  });
+});
