89a4dec958fc3e88b77ce57b475c96ff360e40e6
isamu <isamu@to-kyo.to>
2023-03-11 16:01:37 +0900

    message nft

--- contract/scripts/deploy_message.ts
+++ contract/scripts/deploy_message.ts
@@ -1,5 +1,6 @@
 import { ethers, network } from 'hardhat';
 import { writeFile } from 'fs';
+import { addresses } from '../../src/utils/addresses';
 
 const deploy = async (name: string, args: any) => {
   const factory = await ethers.getContractFactory(name);
@@ -18,30 +19,52 @@ const deployNFT = async (name: string, args: any, args2: any) => {
 };
 async function main() {
   const factorySVGStore = await ethers.getContractFactory('MessageStoreV1');
-  const store = await factorySVGStore.deploy('0x980aAc123617e2B2ea407081Ceb72d5854BAa3D1');
 
+  // const store = await factorySVGStore.deploy(addresses.londrina_solid.mainnet); // mainnet font.
+  console.log("1");
+  const store = await factorySVGStore.deploy(addresses.londrina_solid.goerli); // goerli font.
+
+  console.log("2");
   await store.register({ message: 'hello', color: '#000' });
-  const res = await store.getSVG(1);
+  console.log("2");
+  const res = await store.getSVG(0);
   // console.log(res);
 
+  console.log("3");
   const factoryMessageProvider = await ethers.getContractFactory('MessageProvider2');
-  const provider = await factoryMessageProvider.deploy(store.address);
 
+  // const provider = await factoryMessageProvider.deploy(store.address, addresses.alphabet.mainnet);
+  // const provider = await factoryMessageProvider.deploy(store.address, addresses.pnouns.mainnet);
+
+  // const provider = await factoryMessageProvider.deploy(store.address, addresses.splatter.mainnet);
+  const provider = await factoryMessageProvider.deploy(store.address, addresses.splatter.goerli);
+
+  // const provider = await factoryMessageProvider.deploy(store.address, addresses.snowArt.goerli); // go
+  console.log("4");
+
+
+  console.log("5");
   const factoryMessageToken = await ethers.getContractFactory('MessageToken');
   const token = await factoryMessageToken.deploy(provider.address, store.address);
 
+  console.log("6");
   // mint 0
   await token.mintWithAsset({ message: 'first nft!', color: 'orange' });
   await token.mintWithAsset({ message: 'first nft!', color: 'orange' });
+  console.log("7");
+
   await token.mintWithAsset({ message: 'first nft!', color: 'orange' });
+  console.log("8");
 
   // const res = await store.getSVG(1);
   // console.log(res);
 
+/*
   const nft0 = await token.tokenURI(0);
   console.log('---nft0---');
   console.log(nft0);
-
+*/
+  
   /*
   const nft1 = await token.tokenURI(1);
   console.log("---nft1---");
@@ -57,12 +80,13 @@ async function main() {
   console.log("---nft1---");
   console.log(nft1);
 */
+  /*
   const messageSVG = await store.getSVGMessage('This\n is\n a\n pen', 'pink', { w: 1024, h: 1024 });
   console.log(messageSVG);
 
   const testMessage = await store.test('This\nis\na\npen');
   console.log(testMessage);
-
+  */
   console.log(`store      contract="${store.address}"`);
 
   console.log(`provider      contract="${provider.address}"`);
--- src/views/Message.vue
+++ src/views/Message.vue
@@ -1,66 +1,47 @@
 <template>
   <div class="home">
-    <h2>Store</h2>
+    <h2 class="font-bold text-xl m-4">Fully On-chain Message NFT Token.</h2>
     <div class="mx-8">
       <div>
-        mesasge:
-        <textarea
-          type="text"
-          rows="8"
-          class="w-full resize rounded-md border-2"
-          v-model="storeMessage"
-        />
-      </div>
-      <div class="flex items-center justify-center space-x-8">
-        color:
-        <input type="text" class="w-full rounded-md border-2" v-model="color" />
-        <br />
-      </div>
-      <div>
+        <div class="text-left">
+          Mesasge:
+        </div>
         <div>
-          <button
-            @click="debug"
-            class="mt-2 inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
-          >
-            debug
-          </button>
+          <textarea
+            type="text"
+            rows="8"
+            class="w-full resize rounded-md border-2"
+            v-model="message"
+            />
         </div>
       </div>
-      <div v-if="debugImg">
-        <img :src="debugImg" class="w-36 border-2" />
-      </div>
-    </div>
-
-    <hr class="my-4" />
-    <h2>Token</h2>
-    <div class="mx-8">
-      <div class="flex items-center justify-center space-x-8">
-        mesasge:
-        <textarea
-          type="text"
-          rows="8"
-          class="w-full resize rounded-md border-2"
-          v-model="message"
-        />
-      </div>
-      <div class="flex items-center justify-center space-x-8">
-        color:
-        <input type="text" class="w-full rounded-md border-2" v-model="color" />
+      <div class="text-left mt-4">
+        <span :style="{'color': color}" class="font-bold">Color:</span>
+        <select v-model="color"
+                class="resize rounded-md border-2"
+                >
+          <option v-for="(option, k) in colors" :value="option.value" :key="k">
+            {{ option.text }}
+          </option>
+        </select>
         <br />
       </div>
-
       <NetworkGate :expectedNetwork="chainId">
-        <div class="flex">
+        <div class="flex items-center justify-center">
           <button
             @click="mint"
-            class="mt-2 inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
+            class="w-full mt-4 inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
           >
-            mint
+            <span class="font-bold text-xl">
+              MINT
+            </span>
           </button>
         </div>
-        <div v-for="(token, k) in tokens" :key="k">
-          {{ token.name }}
-          <img :src="token.image" class="w-36 border-2" />
+        <div class="mt-4">
+          <div v-for="(token, k) in tokens" :key="k">
+            <div class="mt-2 font-bold">{{ token.name }}</div>
+            <img :src="token.image" class="w-48 border-2 mt-4" />
+          </div>
         </div>
       </NetworkGate>
     </div>
@@ -91,57 +72,42 @@ export default defineComponent({
     NetworkGate,
   },
   setup(props) {
-    const storeMessage = ref("this\nis\na\npen");
     const message = ref("test");
-    const color = ref("skyblue");
+    const color = ref("orange");
 
-    // store      contract="0x15F2ea83eB97ede71d84Bd04fFF29444f6b7cd52"
-    // provider      contract="0xF357118EBd576f3C812c7875B1A1651a7f140E9C"
-    // token      contract="0x519b05b3655F4b89731B677d64CEcf761f4076f6"
+    const colors = [
+      "pink",
+      "orange",
+      "yellow",
+      "green",
+      "darkmagenta",
+      "aquamarine",
+      "gold",
+      "deeppink",
+      "indigo",
+      "orangered",
+      "red",
+      "mediumblue",
+      "palegreen",
+      "cornflowerblue",
+    ].map(c => ({
+      value: c, text: c,
+    }));      
+    
+    const network = "goerli";
 
-    const network = "localhost";
-    const tokenAddress = "0x519b05b3655F4b89731B677d64CEcf761f4076f6";
-    const storeAddress = "0x15F2ea83eB97ede71d84Bd04fFF29444f6b7cd52";
-
+    // store 0x4e9A9414734f9BcfcDa3b9B07d54D288B5C212A7
+    // provider 0x20Ed6e09220aDE749F14E4eC40fD3B85F7547D9f
+    // token 0x29ef4ee08c5bc5e158b96665c49adf6a26392153
+    const tokenAddress = "0x29ef4ee08c5bc5e158b96665c49adf6a26392153";
+    // const storeAddress = "0x43cA9bAe8dF108684E5EAaA720C25e1b32B0A075";
+    
     const chainId = ChainIdMap[network];
 
     const { networkContext } = useMessageTokenNetworkContext(
       chainId,
       tokenAddress
     );
-
-    const { networkContext: storeContext } = useMessageStoreNetworkContext(
-      chainId,
-      storeAddress
-    );
-
-    const debugImg = ref("");
-    const debug = async () => {
-      if (storeContext.value == null) {
-        return;
-      }
-      const { contract } = storeContext.value;
-      isMinting.value = true;
-
-      try {
-        const res = await contract.functions.getSVGMessage(
-          storeMessage.value || "",
-          color.value,
-          // { w: 1024, h: 512 }
-          { w: 512, h: 1024 }
-        );
-
-        // const result = await tx.wait();
-        // console.log("mint:gasUsed", result.gasUsed.toNumber());
-        debugImg.value =
-          "data:image/svg+xml;base64," + Buffer.from(res[0]).toString("base64");
-        // console.log(img);
-        console.log(res[0]);
-      } catch (e) {
-        console.error(e);
-      }
-      isMinting.value = false;
-    };
 
     const isMinting = ref<boolean>(false);
     const mint = async () => {
@@ -156,42 +122,38 @@ export default defineComponent({
         color: color.value,
       };
       try {
-        console.log(ret);
         const tx = await contract.functions.mintWithAsset(ret);
         console.log("mint:tx");
         const result = await tx.wait();
         console.log("mint:gasUsed", result.gasUsed.toNumber());
       } catch (e) {
+        alert("We are sorry, but something went wrong.");
         console.error(e);
       }
       isMinting.value = false;
     };
-    //
+
     const alchemyKey = process.env.VUE_APP_ALCHEMY_API_KEY;
     const provider = getProvider(network, alchemyKey);
     const tokenContract = getMessageTokenContract(tokenAddress, provider);
     const tokens = ref<any[]>([]);
+
     tokenContract.totalSupply().then(async (nextId: BigNumber) => {
       const token = nextId.toNumber() - 1;
       for (let i = 0; i < 10; i++) {
         if (token - i > 0) {
           const ret = await tokenContract.tokenURI(token - i);
           const data = JSON.parse(atob(ret.split(",")[1]));
-          console.log(data);
           tokens.value.push(data);
         }
       }
     });
 
+    
     return {
       message,
       color,
-
-      // debug
-      debug,
-      storeMessage,
-      debugImg,
-
+      colors,
       // mint
       mint,
       chainId,
