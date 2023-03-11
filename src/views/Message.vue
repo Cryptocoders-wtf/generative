02c0044734cc850eb007dbe5900900fd7cfdbd56
isamu <isamu@to-kyo.to>
2023-03-11 16:02:33 +0900

    format

--- src/views/Message.vue
+++ src/views/Message.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="home">
-    <h2 class="font-bold text-xl m-4">Fully On-chain Message NFT Token.</h2>
+    <h2 class="m-4 text-xl font-bold">Fully On-chain Message NFT Token.</h2>
     <div class="mx-8">
       <div>
-        <div class="text-left">
-          Mesasge:
-        </div>
+        <div class="text-left">Mesasge:</div>
         <div>
           <textarea
             type="text"
             rows="8"
             class="w-full resize rounded-md border-2"
             v-model="message"
-            />
+          />
         </div>
       </div>
-      <div class="text-left mt-4">
-        <span :style="{'color': color}" class="font-bold">Color:</span>
-        <select v-model="color"
-                class="resize rounded-md border-2"
-                >
+      <div class="mt-4 text-left">
+        <span :style="{ color: color }" class="font-bold">Color:</span>
+        <select v-model="color" class="resize rounded-md border-2">
           <option v-for="(option, k) in colors" :value="option.value" :key="k">
             {{ option.text }}
           </option>
@@ -30,17 +26,15 @@
         <div class="flex items-center justify-center">
           <button
             @click="mint"
-            class="w-full mt-4 inline-block rounded bg-green-600 px-6 py-2.5 leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
+            class="mt-4 inline-block w-full rounded bg-green-600 px-6 py-2.5 leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg"
           >
-            <span class="font-bold text-xl">
-              MINT
-            </span>
+            <span class="text-xl font-bold"> MINT </span>
           </button>
         </div>
         <div class="mt-4">
           <div v-for="(token, k) in tokens" :key="k">
             <div class="mt-2 font-bold">{{ token.name }}</div>
-            <img :src="token.image" class="w-48 border-2 mt-4" />
+            <img :src="token.image" class="mt-4 w-48 border-2" />
           </div>
         </div>
       </NetworkGate>
@@ -90,10 +84,11 @@ export default defineComponent({
       "mediumblue",
       "palegreen",
       "cornflowerblue",
-    ].map(c => ({
-      value: c, text: c,
-    }));      
-    
+    ].map((c) => ({
+      value: c,
+      text: c,
+    }));
+
     const network = "goerli";
 
     // store 0x4e9A9414734f9BcfcDa3b9B07d54D288B5C212A7
@@ -101,7 +96,7 @@ export default defineComponent({
     // token 0x29ef4ee08c5bc5e158b96665c49adf6a26392153
     const tokenAddress = "0x29ef4ee08c5bc5e158b96665c49adf6a26392153";
     // const storeAddress = "0x43cA9bAe8dF108684E5EAaA720C25e1b32B0A075";
-    
+
     const chainId = ChainIdMap[network];
 
     const { networkContext } = useMessageTokenNetworkContext(
@@ -149,7 +144,6 @@ export default defineComponent({
       }
     });
 
-    
     return {
       message,
       color,
