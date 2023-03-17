553d515951b25a344d050fb98c7452507d25bffa
isamu <isamu@to-kyo.to>
2023-03-18 04:08:19 +0900

    cleanup SimpleSVGToken

--- contract/contracts/samples/SimpleSVGToken.sol
+++ contract/contracts/samples/SimpleSVGToken.sol
@@ -9,26 +9,16 @@ pragma solidity ^0.8.6;
 import '@openzeppelin/contracts/utils/Strings.sol';
 import '../packages/ERC721P2P/ERC721AP2P.sol';
 import { Base64 } from 'base64-sol/base64.sol';
-import 'randomizer.sol/Randomizer.sol';
 
-import '../packages/graphics/Path.sol';
 import '../packages/graphics/SVG.sol';
 
 contract SimpleSVGToken is ERC721AP2P {
-  using Randomizer for Randomizer.Seed;
   using Strings for uint256;
   using Strings for uint16;
-  using Vector for Vector.Struct;
-  using Path for uint[];
   using SVG for SVG.Element;
 
   uint public nextTokenId;
   
-  struct Props {
-    uint count; // number of spikes
-    uint length; // relative length of each valley (percentile)
-  }
-
   // To be specified by the concrete contract
   string public description;
   uint public mintPrice;
@@ -43,14 +33,6 @@ contract SimpleSVGToken is ERC721AP2P {
     
   }
 
-  function setDescription(string memory _description) external onlyOwner {
-    description = _description;
-  }
-
-  function setMintPrice(uint256 _price) external onlyOwner {
-    mintPrice = _price;
-  }
-
   function tokenName(uint256 _tokenId) internal pure returns (string memory) {
     return string(abi.encodePacked('Simple SVG Token ', _tokenId.toString()));
   }
@@ -62,35 +44,29 @@ contract SimpleSVGToken is ERC721AP2P {
     return nextTokenId++;
   }
 
-  function mintPriceFor(address) public view virtual returns (uint256) {
-    return mintPrice;
-  }
-
   function totalSupply() public view override returns (uint256) {
     return nextTokenId;
   }
   // for graphics
+
+  // Implement SVG here
+  // https://github.com/Cryptocoders-wtf/generative/tree/main/contract/contracts/packages/graphicss#basic-concept
+  function generateSVGPart(uint256 _tokenId) public pure returns (string memory svgPart) {
+     SVG.Element memory path = SVG.rect(256, 256, 512, 512)
+         .fill("yellow")
+         .stroke("blue", 10)
+         .transform("rotate(30 512 512)");
+         
+     svgPart = string(path.svg());
+  }
 
   string constant SVGHeader =
     '<svg viewBox="0 0 1024 1024'
     '"  xmlns="http://www.w3.org/2000/svg">\n'
     '<g>\n';
 
-  function debugGenerateSVG(uint256 _assetId) external pure returns (string memory) {
-    string memory svgPart = generateSVGPart(_assetId);
-    return
-      string(
-        abi.encodePacked(
-          SVGHeader,
-          svgPart,
-          '</g>\n'
-          '</svg>\n'
-        )
-      );
-  }
-  
-  function generateSVG(uint256 _assetId) internal pure returns (string memory) {
-    string memory svgPart = generateSVGPart(_assetId);
+  function debugGenerateSVG(uint256 _tokenId) external pure returns (string memory) {
+    string memory svgPart = generateSVGPart(_tokenId);
     return
       string(
         abi.encodePacked(
@@ -103,10 +79,16 @@ contract SimpleSVGToken is ERC721AP2P {
   }
   
   function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-    bytes memory image = bytes(generateSVG(_tokenId));
+      string memory svgPart = generateSVGPart(_tokenId);
+      bytes memory image = abi.encodePacked(
+                                            SVGHeader,
+                                            svgPart,
+                                            '</g>\n'
+                                            '</svg>\n'
+                                            );
 
-    return
-      string(
+
+      return string(
         abi.encodePacked(
           'data:application/json;base64,',
           Base64.encode(
@@ -126,13 +108,5 @@ contract SimpleSVGToken is ERC721AP2P {
       );
   }
 
- function generateSVGPart(uint256 _assetId) public pure returns (string memory svgPart) {
-     SVG.Element memory path = SVG.rect(256, 256, 512, 512)
-         .fill("yellow")
-         .stroke("blue", 10)
-         .transform("rotate(30 512 512)");
-         
-     svgPart = string(path.svg());
-  }
 
 }
