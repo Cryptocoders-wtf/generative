9f5d8321046aabf78c3b765e53adff7b0615f446
isamu <isamu@to-kyo.to>
2023-03-17 09:02:04 +0900

    add SimpleSVGToken for sample

---
+++ contract/scripts/deploy_simpleSVG.ts
@@ -0,0 +1,25 @@
+import { ethers, network } from 'hardhat';
+import { writeFile } from 'fs';
+import { addresses } from '../../src/utils/addresses';
+
+async function main() {
+  const factory = await ethers.getContractFactory('SimpleSVGToken');
+  const contract = await factory.deploy("aa", "AA");
+  await contract.deployed();
+  console.log(`      simple="${contract.address}"`);
+  
+  await contract.mint();
+  await contract.mint();
+  await contract.mint();
+  await contract.mint();
+
+  const res = await contract.tokenURI(1);
+  console.log(res);
+  const res2 = await contract.debugGenerateSVG(1);
+  console.log(res2);
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
