b1ce59309d7939e63d0bc224c7c97947b62e7086
isamu <isamu@to-kyo.to>
2023-04-15 18:56:41 +0900

    removed routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,22 +4,10 @@ import Blank from "@/components/Blank.vue";
 import NotFound from "@/components/NotFound.vue";
 
 import Home from "@/views/Home.vue";
-import Splatter from "@/views/Splatter.vue";
-import Snow from "@/views/Snow.vue";
-import Nouns from "@/views/Nouns.vue";
-import Paper from "@/views/Paper.vue";
-import Bitcoin from "@/views/Bitcoin.vue";
-import Reddit from "@/views/Reddit.vue";
-import Sample from "@/views/Sample.vue";
-import Experiment from "@/views/Experiment.vue";
-import Star from "@/views/Star.vue";
-import Alphabet from "@/views/Alphabet.vue";
 
 import Create from "@/views/Create.vue";
 import Items from "@/views/Items.vue";
 import SvgTest from "@/views/SVGTest.vue";
-import Message from "@/views/Message.vue";
-import MessagePNouns from "@/views/MessagePNouns.vue";
 
 import P2SeaItem from "@/views/P2SeaItem.vue";
 
@@ -29,60 +17,12 @@ const routeChildren: Array<RouteRecordRaw> = [
     component: Create,
   },
   {
-    path: "splatter",
-    component: Splatter,
-  },
-  {
-    path: "snow",
-    component: Snow,
-  },
-  {
-    path: "nouns",
-    component: Nouns,
-  },
-  {
-    path: "paper",
-    component: Paper,
-  },
-  {
-    path: "bitcoin",
-    component: Bitcoin,
-  },
-  {
-    path: "reddit",
-    component: Reddit,
-  },
-  {
-    path: "exp",
-    component: Experiment,
-  },
-  {
-    path: "star",
-    component: Star,
-  },
-  {
     path: "svg",
     component: Create,
   },
   {
     path: "svgtest",
     component: SvgTest,
-  },
-  {
-    path: "message",
-    component: Message,
-  },
-  {
-    path: "message_pnouns",
-    component: MessagePNouns,
-  },
-  {
-    path: "alphabet",
-    component: Alphabet,
-  },
-  {
-    path: "sample",
-    component: Sample,
   },
   {
     path: "item/:token_id",
