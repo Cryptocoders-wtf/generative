33b28194803334757e8fe7179f78f8962e1645f0
togawa manabu <togawamanabu@gmail.com>
2023-04-15 16:16:26 +0900

    added hero section in create

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="home">
+
+    <section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Convert SVG with simple structure to full on-chain NFT</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">All data is converted to the d attribute of the path element. <br/>
+            Only circle, ellipses, line, rect, polygon, polyline element are converted to the d attribute of the path element. All other elements are ignored.</p>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img class="rounded-2xl" src="@/assets/heroimg.png" alt="mockup">
+        </div>                
+    </div>
+</section>
+
     <div class="flex items-center justify-center space-x-8">
       <!-- Drag & Drop -->
       <div
