b13afb77597b9f0697c2dbe2385ba58c4db19055
isamu <isamu@to-kyo.to>
2023-05-15 06:33:34 +0900

    format

--- src/utils/addresses/noto_sans_goerli.ts
+++ src/utils/addresses/noto_sans_goerli.ts
@@ -1,3 +1,3 @@
 export const addresses = {
-  font:"0xf4fd7f7e9fC89B2F539BE378214EbB768ECBf91B",
-}
+  font: "0xf4fd7f7e9fC89B2F539BE378214EbB768ECBf91B",
+};
--- src/utils/addresses/noto_sans_mumbai.ts
+++ src/utils/addresses/noto_sans_mumbai.ts
@@ -1,3 +1,3 @@
 export const addresses = {
-  font:"0xCBbC30D7d8b9DCF720f4FfB98Ef42E5A2633B791",
-}
+  font: "0xCBbC30D7d8b9DCF720f4FfB98Ef42E5A2633B791",
+};
