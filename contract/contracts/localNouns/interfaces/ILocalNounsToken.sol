c528f8a605792e1f9859820099b984a730af5c26
eibakatsu <eiba.katsu@gmail.com>
2023-10-25 09:17:04 +0900

    mintSelectedPrefectureBatch削除

--- contract/contracts/localNouns/interfaces/ILocalNounsToken.sol
+++ contract/contracts/localNouns/interfaces/ILocalNounsToken.sol
@@ -20,12 +20,6 @@ pragma solidity ^0.8.6;
 interface ILocalNounsToken {
   function mintSelectedPrefecture(address to, uint256 prefectureId, uint256 _amount) external returns (uint256 tokenId);
 
-  function mintSelectedPrefectureBatch(
-    address _to,
-    uint256[] memory _prefectureId,
-    uint256[] memory _amount
-  ) external returns (uint256 tokenId);
-
   function setMinter(address _minter) external;
 
   // Fires when the owner puts the trade
