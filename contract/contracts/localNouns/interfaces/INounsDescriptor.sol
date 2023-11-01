872fd3d65c5ea27d8af4b0a2855a9b7dff246691
eibakatsu <eiba.katsu@gmail.com>
2023-11-01 15:03:52 +0900

    format

--- contract/contracts/localNouns/interfaces/INounsDescriptor.sol
+++ contract/contracts/localNouns/interfaces/INounsDescriptor.sol
@@ -21,87 +21,87 @@ import { INounsSeeder } from './INounsSeeder.sol';
 import { INounsDescriptorMinimal } from './INounsDescriptorMinimal.sol';
 
 interface INounsDescriptor is INounsDescriptorMinimal {
-    event PartsLocked();
+  event PartsLocked();
 
-    event DataURIToggled(bool enabled);
+  event DataURIToggled(bool enabled);
 
-    event BaseURIUpdated(string baseURI);
+  event BaseURIUpdated(string baseURI);
 
-    function arePartsLocked() external returns (bool);
+  function arePartsLocked() external returns (bool);
 
-    function isDataURIEnabled() external returns (bool);
+  function isDataURIEnabled() external returns (bool);
 
-    function baseURI() external returns (string memory);
+  function baseURI() external returns (string memory);
 
-    function palettes(uint8 paletteIndex, uint256 colorIndex) external view returns (string memory);
+  function palettes(uint8 paletteIndex, uint256 colorIndex) external view returns (string memory);
 
-    function backgrounds(uint256 index) external view returns (string memory);
+  function backgrounds(uint256 index) external view returns (string memory);
 
-    function bodies(uint256 index) external view returns (bytes memory);
+  function bodies(uint256 index) external view returns (bytes memory);
 
-    function accessories(uint256 index) external view returns (bytes memory);
+  function accessories(uint256 index) external view returns (bytes memory);
 
-    function heads(uint256 index) external view returns (bytes memory);
+  function heads(uint256 index) external view returns (bytes memory);
 
-    function glasses(uint256 index) external view returns (bytes memory);
+  function glasses(uint256 index) external view returns (bytes memory);
 
-    function backgroundCount() external view override returns (uint256);
+  function backgroundCount() external view override returns (uint256);
 
-    function bodyCount() external view override returns (uint256);
+  function bodyCount() external view override returns (uint256);
 
-    function accessoryCount() external view override returns (uint256);
+  function accessoryCount() external view override returns (uint256);
 
-    function accessoryCountInPrefecture(uint256 prefectureId) external view returns (uint256);
+  function accessoryCountInPrefecture(uint256 prefectureId) external view returns (uint256);
 
-    function accessoryName(uint256 partsId) external view returns (string memory);
-    
-    function headCount() external view override returns (uint256);
+  function accessoryName(uint256 partsId) external view returns (string memory);
 
-    function headCountInPrefecture(uint256 prefectureId) external view returns (uint256);
+  function headCount() external view override returns (uint256);
 
-    function headName(uint256 partsId) external view returns (string memory);
+  function headCountInPrefecture(uint256 prefectureId) external view returns (uint256);
 
-    function glassesCount() external view override returns (uint256);
+  function headName(uint256 partsId) external view returns (string memory);
 
-    function addManyColorsToPalette(uint8 paletteIndex, string[] calldata newColors) external;
+  function glassesCount() external view override returns (uint256);
 
-    function addManyBackgrounds(string[] calldata backgrounds) external;
+  function addManyColorsToPalette(uint8 paletteIndex, string[] calldata newColors) external;
 
-    function addManyBodies(bytes[] calldata bodies) external;
+  function addManyBackgrounds(string[] calldata backgrounds) external;
 
-    function addManyAccessories(uint256 prefectureId, bytes[] calldata accessories, string[] calldata name) external;
+  function addManyBodies(bytes[] calldata bodies) external;
 
-    function addManyHeads(uint256 prefectureId, bytes[] calldata heads, string[] calldata name) external;
+  function addManyAccessories(uint256 prefectureId, bytes[] calldata accessories, string[] calldata name) external;
 
-    function addManyGlasses(bytes[] calldata glasses) external;
+  function addManyHeads(uint256 prefectureId, bytes[] calldata heads, string[] calldata name) external;
 
-    function addColorToPalette(uint8 paletteIndex, string calldata color) external;
+  function addManyGlasses(bytes[] calldata glasses) external;
 
-    function addBackground(string calldata background) external;
+  function addColorToPalette(uint8 paletteIndex, string calldata color) external;
 
-    function addBody(bytes calldata body) external;
+  function addBackground(string calldata background) external;
 
-    function addAccessory(uint256 prefectureId, bytes calldata accessory, string calldata name) external;
+  function addBody(bytes calldata body) external;
 
-    function addHead(uint256 prefectureId, bytes calldata head, string calldata name) external;
+  function addAccessory(uint256 prefectureId, bytes calldata accessory, string calldata name) external;
 
-    function addGlasses(bytes calldata glasses) external;
+  function addHead(uint256 prefectureId, bytes calldata head, string calldata name) external;
 
-    function lockParts() external;
+  function addGlasses(bytes calldata glasses) external;
 
-    function toggleDataURIEnabled() external;
+  function lockParts() external;
 
-    function setBaseURI(string calldata baseURI) external;
+  function toggleDataURIEnabled() external;
 
-    function tokenURI(uint256 tokenId, INounsSeeder.Seed memory seed) external view override returns (string memory);
+  function setBaseURI(string calldata baseURI) external;
 
-    function dataURI(uint256 tokenId, INounsSeeder.Seed memory seed) external view override returns (string memory);
+  function tokenURI(uint256 tokenId, INounsSeeder.Seed memory seed) external view override returns (string memory);
 
-    function genericDataURI(
-        string calldata name,
-        string calldata description,
-        INounsSeeder.Seed memory seed
-    ) external view returns (string memory);
+  function dataURI(uint256 tokenId, INounsSeeder.Seed memory seed) external view override returns (string memory);
 
-    function generateSVGImage(INounsSeeder.Seed memory seed) external view returns (string memory);
+  function genericDataURI(
+    string calldata name,
+    string calldata description,
+    INounsSeeder.Seed memory seed
+  ) external view returns (string memory);
+
+  function generateSVGImage(INounsSeeder.Seed memory seed) external view returns (string memory);
 }
