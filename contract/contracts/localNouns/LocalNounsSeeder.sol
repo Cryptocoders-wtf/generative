e0b1cd0ef4a6f775302cf39c92ab0610eab6673a
eibakatsu <eiba.katsu@gmail.com>
2023-11-29 17:34:54 +0900

    エラー回避のためkeccak256使用を回避

--- contract/contracts/localNouns/LocalNounsSeeder.sol
+++ contract/contracts/localNouns/LocalNounsSeeder.sol
@@ -21,26 +21,22 @@ import { ILocalNounsSeeder } from './interfaces/ILocalNounsSeeder.sol';
 import { INounsDescriptorMinimal } from './interfaces/INounsDescriptorMinimal.sol';
 
 contract LocalNounsSeeder is ILocalNounsSeeder {
-    /**
-     * @notice Generate a pseudo-random Noun seed using the previous blockhash and noun ID.
-     */
-    function generateSeed(uint256 prefectureId, uint256 nounId, INounsDescriptorMinimal descriptor) external view returns (Seed memory) {
-        uint256 pseudorandomness = uint256(
-            keccak256(abi.encodePacked(blockhash(block.number - 1), nounId))
-        );
+  /**
+   * @notice Generate a pseudo-random Noun seed using the previous blockhash and noun ID.
+   */
+  function generateSeed(
+    uint256 prefectureId,
+    uint256 nounId,
+    INounsDescriptorMinimal descriptor
+  ) external view returns (Seed memory) {
+    uint256 pseudorandomness = (block.number - 1) + nounId;
 
-        uint256 accessoryCount = descriptor.accessoryCountInPrefecture(prefectureId % 100); // 1,2桁目：都道府県番号、3桁目以降：バージョン番号
-        uint256 headCount = descriptor.headCountInPrefecture(prefectureId);
+    uint256 accessoryCount = descriptor.accessoryCountInPrefecture(prefectureId % 100); // 1,2桁目：都道府県番号、3桁目以降：バージョン番号
+    uint256 headCount = descriptor.headCountInPrefecture(prefectureId);
 
-        uint256 accesoryPartId = descriptor.accessoryInPrefecture(prefectureId % 100, (pseudorandomness >> 96) % accessoryCount);
-        uint256 headPartId = descriptor.headInPrefecture(prefectureId, (pseudorandomness >> 144) % headCount);
+    uint256 accesoryPartId = descriptor.accessoryInPrefecture(prefectureId % 100, pseudorandomness % accessoryCount);
+    uint256 headPartId = descriptor.headInPrefecture(prefectureId, pseudorandomness % headCount);
 
-        return Seed({
-            background: 0,
-            body: 0,
-            accessory: uint48(accesoryPartId),
-            head: uint48(headPartId),
-            glasses: 0
-        });
-    }
+    return Seed({ background: 0, body: 0, accessory: uint48(accesoryPartId), head: uint48(headPartId), glasses: 0 });
+  }
 }
